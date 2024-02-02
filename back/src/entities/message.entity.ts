65dcc242ce03f5ab2992d8ecf76063f23cf6c55f
AlexandreOttmann <ottmann.alex@gmail.com>
2024-02-02 14:59:44 +0100

    BACK-ECOVOIT-0008 FIX:Update Message

--- back/src/entities/message.entity.ts
+++ back/src/entities/message.entity.ts
@@ -1,10 +1,13 @@
 import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, In } from "typeorm";
-import { Field, InputType, ObjectType, ID } from "type-graphql";
+import { Field, InputType, ObjectType, ID, GraphQLISODateTime } from "type-graphql";
 import { Length, Min } from "class-validator";
 
 
 import { UserEntity } from "./user.entity";
 import { JourneyEntity } from "./journey.entity";
+
+
+export type Status = "ACTIVE" | "ARCHIVED";
 
 @ObjectType()
 @Entity()
@@ -17,8 +20,22 @@ export class MessageEntity {
   @Column()
   content: string;
 
+  @Field(() => GraphQLISODateTime)
+  @CreateDateColumn()
+  modifiedAt: string;
+
+  @Field(() => GraphQLISODateTime)
   @CreateDateColumn()
   createdAt: string;
+
+  @Field()
+  @Column({
+    type: "text",
+    enum: ["ACTIVE", "ARCHIVED"],
+    default: "ACTIVE",
+  })
+  status: Status;
+
 
   @ManyToOne(() => UserEntity, u => u.messages)
   user: UserEntity;
@@ -27,7 +44,7 @@ export class MessageEntity {
   journey: JourneyEntity;
 }
 
-// INPUT
+// INPUT PARTIALS
 
 @InputType()
 export class PartialUserInput {
@@ -39,6 +56,7 @@ export class PartialJourneyInput {
   @Field(() => ID)
   id: string;
 }
+
 // CREATE 
 @InputType()
 export class CreateMessageInput {
@@ -48,6 +66,29 @@ export class CreateMessageInput {
 
   @Field(() => Date)
   created_at: Date;
+
+  @Field(() => Date)
+  modifed_at: Date;
+
+  @Field(() => ID)
+  user: PartialUserInput;
+
+  @Field(() => ID)
+  journey: PartialJourneyInput;
+}
+
+// UPDATE
+@InputType()
+export class UpdateMessageInput {
+  @Field()
+  @Length(1, 500, { message: "Le message doit contenir au minimum 1 caractère et au maximum 500 caractères" })
+  content: string;
+
+  @Field({ nullable: true })
+  status: Status;
+
+  @Field(() => Date)
+  modifed_at: Date;
 
   @Field(() => ID)
   user: PartialUserInput;
