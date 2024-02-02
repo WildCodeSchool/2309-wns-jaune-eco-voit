10e3db868ecb92d34dd2db66d3c6917367064d14
lourima <marielou.lejan@gmail.com>
2024-02-02 12:19:23 +0100

    review

--- back/src/entities/user.entity.ts
+++ back/src/entities/user.entity.ts
@@ -1,80 +1,79 @@
-import { Field, ID, ObjectType } from "type-graphql";
+import { EmailAddress, PhoneNumber } from "graphql-scalars/typings/mocks";
+import { Field, GraphQLISODateTime, ID, ObjectType } from "type-graphql";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-
 export type Role = "USER" | "ADMIN";
-export type Grade = 'BEGINNER' | 'CONFIRMED' | 'AMBASSADOR';
-export type Status = 'ARCHIVED' | 'ACTIVE';
+export type Grade = "BEGINNER" | "CONFIRMED" | "AMBASSADOR";
+export type Status = "ARCHIVED" | "ACTIVE";
 
-@ObjectType()  
+@ObjectType()
 @Entity()
 export class UserEntity {
-    @Field(() => ID)
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+  @Field(() => ID)
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Field()
-    @Column()
-    firstname: string
+  @Field()
+  @Column({ length: 50 })
+  firstname: string;
 
-    @Field()
-    @Column()
-    lastname: string;
+  @Field()
+  @Column({ length: 50 })
+  lastname: string;
 
-    @Field()
-    @Column()
-    email: string
+  @Field(() => EmailAddress)
+  @Column({ length: 50 })
+  email: string;
 
-    @Field()
-    @Column()
-    password: string
-    
-    @Field()
-    @Column({type: 'timestamptz'})
-    dateOfBirth: Date
+  @Field()
+  @Column()
+  password: string;
 
-    @Field()
-    @Column()
-    phoneNumber: string
-    
-    @Field()
-    @Column()
-    profilPicture: string
+  @Field(() => GraphQLISODateTime)
+  @Column({ type: "timestamptz" })
+  dateOfBirth: Date;
 
-    @Field()
-    @Column({
-        type: 'text', 
-        enum: ['ADMIN', 'USER'],
-        default: 'USER',
-    })
-    role: Role
+  @Field(() => PhoneNumber)
+  @Column()
+  phoneNumber: string;
 
-    @Field()
-    @Column({
-        type: 'text', 
-        enum: ['BEGINNER', 'CONFIRMED', 'AMBASSADOR'],
-        default: 'BEGINNER',
-    })
-    grade: Grade
+  @Field()
+  @Column()
+  profilPicture: string;
 
-    @Field()
-    @Column()
-    tripsAsPassenger: number
+  @Field()
+  @Column({
+    type: "text",
+    enum: ["ADMIN", "USER"],
+    default: "USER",
+  })
+  role: Role;
 
-    @Field()
-    @Column()
-    tripsAsDriver: number
+  @Field()
+  @Column({
+    type: "text",
+    enum: ["BEGINNER", "CONFIRMED", "AMBASSADOR"],
+    default: "BEGINNER",
+  })
+  grade: Grade;
 
-    @Field()
-    @Column({
-        type: 'text', 
-        enum: ['ACTIVE', 'ARCHIVED'],
-        default: 'ACTIVE',
-    })
-    status: Status
+  @Field()
+  @Column({ default: 0 })
+  tripsAsPassenger: number;
 
-    @Field()
-    @Column()
-    createdAt: Date
+  @Field()
+  @Column({ default: 0 })
+  tripsAsDriver: number;
 
+  @Field()
+  @Column({
+    type: "text",
+    enum: ["ACTIVE", "ARCHIVED"],
+    default: "ACTIVE",
+  })
+  status: Status;
+
+  @Field()
+  @Column()
+  createdAt: Date;
 }
