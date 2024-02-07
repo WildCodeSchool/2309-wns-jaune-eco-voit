c34bb90b051bc7d7a499b8102c1b92eb1d240745
lourima <marielou.lejan@gmail.com>
2024-02-07 17:10:01 +0100

    add userResolver

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,41 +1,40 @@
-import express from "express";
-import { buildSchema } from "type-graphql";
-import http from "http";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import { expressMiddleware } from "@apollo/server/express4";
-import cors from "cors";
+import express from 'express'
+import { buildSchema } from 'type-graphql'
+import http from 'http'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4'
+import cors from 'cors'
 
 import BookingResolver from './resolvers/booking.resolver'
 // import UserResolver from './resolvers/user.resolver'
 // import JourneyResolver from './resolvers/journey.resolver'
-import { ApolloServer } from "@apollo/server";
-import { resolve } from "path";
-import db from "./db";
+import { ApolloServer } from '@apollo/server'
+import db from './db'
+import UserResolver from './resolvers/user.resolver'
 
+const app = express()
+const httpServer = http.createServer(app) // on créer un server HTTP à partir de la bibliothéque d'express, pour avoir Req et Res (pour les middlwares)
 
-const app = express();
-const httpServer = http.createServer(app); // on créer un server HTTP à partir de la bibliothéque d'express, pour avoir Req et Res (pour les middlwares)
-
-async function main(){
+async function main() {
     const schema = await buildSchema({
-        resolvers: [BookingResolver],
+        resolvers: [BookingResolver, UserResolver],
         validate: false,
     })
     const server = new ApolloServer({
         schema,
-        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })] // Informe Apollo Server, qu'il utilisera le server Http créer plus haut
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })], // Informe Apollo Server, qu'il utilisera le server Http créer plus haut
     })
     // lancement du server
-    await server.start();
+    await server.start()
     app.use(
-        "/", 
-        cors<cors.CorsRequest>({ origin: "*" }), // autorise toutes les origines à accéder à l'API. En spécifiant { origin: "*" }, cela permet à n'importe quel domaine d'accéder à l'API
-        express.json(), 
-        expressMiddleware(server, {}), // intégre Apollo Server à Express
+        '/',
+        cors<cors.CorsRequest>({ origin: '*' }), // autorise toutes les origines à accéder à l'API. En spécifiant { origin: "*" }, cela permet à n'importe quel domaine d'accéder à l'API
+        express.json(),
+        expressMiddleware(server, {}) // intégre Apollo Server à Express
     )
-        await db.initialize();
-        await new Promise<void>((resolve) => 
-        httpServer.listen({ port: 4000 }, resolve )
-        );
+    await db.initialize()
+    await new Promise<void>((resolve) =>
+        httpServer.listen({ port: 4000 }, resolve)
+    )
 }
 main()
