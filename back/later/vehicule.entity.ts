ae9ad3a7266d3d888323f8da4c14983802ac0631
Ashitaaka <113708289+Ashitaaka@users.noreply.github.com>
2024-02-07 11:34:09 +0100

    Add ESLint, prettier and commenting unused entities
    - Add ESLint and Prettier
    - Edit readme.md : "npm run format"
    - Comment all unused entities and imports

--- back/later/vehicule.entity.ts
+++ back/later/vehicule.entity.ts
@@ -1,101 +1,101 @@
-import { Length } from "class-validator";
+/*import { Length } from 'class-validator'
 import {
-  Field,
-  GraphQLISODateTime,
-  ID,
-  InputType,
-  ObjectType,
-} from "type-graphql";
+    Field,
+    GraphQLISODateTime,
+    ID,
+    InputType,
+    ObjectType,
+} from 'type-graphql'
 import {
-  Entity,
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  Column,
-  ManyToOne,
-  OneToMany,
-  UpdateDateColumn,
-} from "typeorm";
-import { UserEntity } from "../src/entities/user.entity";
-import { JourneyEntity } from "../src/entities/journey.entity";
+    Entity,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    Column,
+    ManyToOne,
+    OneToMany,
+    UpdateDateColumn,
+} from 'typeorm'
+import { UserEntity } from '../src/entities/user.entity'
+import { JourneyEntity } from '../src/entities/journey.entity'
 
-type VehiculeStatus = "ARCHIVE" | "ACTIVE";
+type VehiculeStatus = 'ARCHIVE' | 'ACTIVE'
 
 @ObjectType()
 @Entity()
 export class VehiculeEntity {
-  @Field(() => ID)
-  @PrimaryGeneratedColumn("uuid")
-  id: string;
+    @Field(() => ID)
+    @PrimaryGeneratedColumn('uuid')
+    id: string
 
-  @Field()
-  @Column({ length: 10 })
-  @Length(10, 10, {
-    message: "La plaque d'immatriculation doit avoir une length de 10",
-  })
-  registration: string;
+    @Field()
+    @Column({ length: 10 })
+    @Length(10, 10, {
+        message: "La plaque d'immatriculation doit avoir une length de 10",
+    })
+    registration: string
 
-  @Field()
-  @Column({ length: 50 })
-  brand: string;
+    @Field()
+    @Column({ length: 50 })
+    brand: string
 
-  @Field()
-  @Column({ length: 50 })
-  model: string;
+    @Field()
+    @Column({ length: 50 })
+    model: string
 
-  @Field(() => UserEntity)
-  @ManyToOne(() => UserEntity, (user) => user.vehicules)
-  user: UserEntity;
+    @Field(() => UserEntity)
+    @ManyToOne(() => UserEntity, (user) => user.vehicules)
+    user: UserEntity
 
-  @Field(() => [JourneyEntity])
-  @OneToMany(() => JourneyEntity, (journey) => journey.vehicule)
-  journeys: JourneyEntity[];
+    @Field(() => [JourneyEntity])
+    @OneToMany(() => JourneyEntity, (journey) => journey.vehicule)
+    journeys: JourneyEntity[]
 
-  @Field()
-  @Column({
-    type: "text",
-    enum: ["ARCHIVE", "ACTIVE"],
-    default: "ACTIVE",
-  })
-  status: VehiculeStatus;
+    @Field()
+    @Column({
+        type: 'text',
+        enum: ['ARCHIVE', 'ACTIVE'],
+        default: 'ACTIVE',
+    })
+    status: VehiculeStatus
 
-  @Field(() => GraphQLISODateTime)
-  @CreateDateColumn()
-  createdAt: Date;
+    @Field(() => GraphQLISODateTime)
+    @CreateDateColumn()
+    createdAt: Date
 
-  @Field(() => GraphQLISODateTime)
-  @UpdateDateColumn()
-  updatedAt: Date;
-}
+    @Field(() => GraphQLISODateTime)
+    @UpdateDateColumn()
+    updatedAt: Date
+}*/
 
 /**============================================
  *?               Inputs
  *=============================================**/
-@InputType()
+/*@InputType()
 export class PartialUserInput {
-  @Field(() => ID)
-  id: string;
+    @Field(() => ID)
+    id: string
 }
 
 @InputType()
 export class CreateVehiculeInput {
-  @Field()
-  registration: string;
-  @Field()
-  brand: string;
-  @Field()
-  model: string;
-  @Field(() => ID)
-  user: PartialUserInput;
+    @Field()
+    registration: string
+    @Field()
+    brand: string
+    @Field()
+    model: string
+    @Field(() => ID)
+    user: PartialUserInput
 }
 
 @InputType()
 export class UpdateVehiculeInput {
-  @Field()
-  id: string;
-  @Field({ nullable: true })
-  registration: string;
-  @Field({ nullable: true })
-  brand: string;
-  @Field({ nullable: true })
-  model: string;
-}
+    @Field()
+    id: string
+    @Field({ nullable: true })
+    registration: string
+    @Field({ nullable: true })
+    brand: string
+    @Field({ nullable: true })
+    model: string
+}*/
--- back/src/entities/booking.entity.ts
+++ back/src/entities/booking.entity.ts
@@ -1,114 +1,108 @@
 import {
-  Entity,
-  Column,
-  Generated,
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  ManyToMany,
-  JoinTable,
-  ManyToOne,
-  OneToMany,
-  UpdateDateColumn,
-} from "typeorm"; // Pour définir les entités TypeORM
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    ManyToOne,
+    UpdateDateColumn,
+} from 'typeorm' // Pour définir les entités TypeORM
 import {
-  Field,
-  Float,
-  GraphQLISODateTime,
-  ID,
-  InputType,
-  ObjectType,
-} from "type-graphql"; // Pour définir les Types GraphQL
-import { Length, Min, min } from "class-validator"; // to add validators
-import { StepEntity } from "../../later/step.entity";
-import { UserEntity } from "./user.entity";
-import { JourneyEntity } from "./journey.entity";
-import { RatingEntity } from "../../later/rating.entity";
+    Field,
+    Float,
+    GraphQLISODateTime,
+    ID,
+    InputType,
+    ObjectType,
+} from 'type-graphql' // Pour définir les Types GraphQL
+import { Min } from 'class-validator' // to add validators
+import { UserEntity } from './user.entity'
+import { JourneyEntity } from './journey.entity'
 
-export type Status = "PENDING" | "REJECTED" | "CANCELLED" | "ACCEPTED";
+export type Status = 'PENDING' | 'REJECTED' | 'CANCELLED' | 'ACCEPTED'
 
 @ObjectType() //pour TypeGraphQL
 @Entity() // pour TypeORM
 export class BookingEntity {
-  @Field(() => ID) // pour GraphQL
-  @PrimaryGeneratedColumn("uuid") // pour TypeORM
-  id: string;
+    @Field(() => ID) // pour GraphQL
+    @PrimaryGeneratedColumn('uuid') // pour TypeORM
+    id: string
 
-  @Field(() => Float)
-  @Column({ type: "float" })
-  @Min(0.1)
-  totalPrice: number;
+    @Field(() => Float)
+    @Column({ type: 'float' })
+    @Min(0.1)
+    totalPrice: number
 
-  @Field()
-  @Column({ type: "timestamptz" }) // Recommended for Date  typeORM
-  departureTime: Date;
+    @Field()
+    @Column({ type: 'timestamptz' }) // Recommended for Date  typeORM
+    departureTime: Date
 
-  //To add an ENUM to a field use options in @Column()
-  @Field()
-  @Column({
-    type: "text",
-    enum: ["PENDING", "REJECTED", "ACCEPTED", "CANCELLED"],
-  })
-  status: Status; // Type créé pour le Statut
+    //To add an ENUM to a field use options in @Column()
+    @Field()
+    @Column({
+        type: 'text',
+        enum: ['PENDING', 'REJECTED', 'ACCEPTED', 'CANCELLED'],
+    })
+    status: Status // Type créé pour le Statut
 
-  @Field(() => [StepEntity])
-  @JoinTable()
-  @ManyToMany(() => StepEntity, (s) => s.bookings)
-  steps: StepEntity[];
+    // @Field(() => [StepEntity])
+    // @JoinTable()
+    // @ManyToMany(() => StepEntity, (s) => s.bookings)
+    // steps: StepEntity[]
 
-  @Field(() => UserEntity)
-  @ManyToOne(() => UserEntity, (u) => u.bookings)
-  user: UserEntity;
+    @Field(() => UserEntity)
+    @ManyToOne(() => UserEntity, (u) => u.bookings)
+    user: UserEntity
 
-  @Field(() => JourneyEntity)
-  @ManyToOne(() => JourneyEntity, (j) => j.bookings)
-  journey: JourneyEntity;
+    @Field(() => JourneyEntity)
+    @ManyToOne(() => JourneyEntity, (j) => j.bookings)
+    journey: JourneyEntity
 
-  @Field(() => [RatingEntity])
-  @OneToMany(() => RatingEntity, (rating) => rating.booking)
-  ratings: RatingEntity[];
+    // @Field(() => [RatingEntity])
+    // @OneToMany(() => RatingEntity, (rating) => rating.booking)
+    // ratings: RatingEntity[]
 
-  @Field(() => GraphQLISODateTime)
-  @CreateDateColumn()
-  createdAt: Date;
+    @Field(() => GraphQLISODateTime)
+    @CreateDateColumn()
+    createdAt: Date
 
-  @Field(() => GraphQLISODateTime)
-  @UpdateDateColumn()
-  updatedAt: Date;
+    @Field(() => GraphQLISODateTime)
+    @UpdateDateColumn()
+    updatedAt: Date
 }
 
 // --------- INPUTS ------------ //
 @InputType()
 export class PartialBookingInput {
-  @Field(() => ID)
-  id: string;
+    @Field(() => ID)
+    id: string
 }
 
 @InputType()
 export class CreateBookingInput {
-  @Field(() => Float)
-  totalPrice: number;
+    @Field(() => Float)
+    totalPrice: number
 
-  @Field()
-  departureTime: Date;
+    @Field()
+    departureTime: Date
 
-  @Field()
-  status: Status;
+    @Field()
+    status: Status
 
-  @Field()
-  steps: PartialBookingInput[];
+    @Field()
+    steps: PartialBookingInput[]
 
-  @Field()
-  user: PartialBookingInput;
+    @Field()
+    user: PartialBookingInput
 
-  @Field()
-  journey: PartialBookingInput;
+    @Field()
+    journey: PartialBookingInput
 }
 
 @InputType()
 export class UpdateBookingInput {
-  @Field(() => ID)
-  id: string;
+    @Field(() => ID)
+    id: string
 
-  @Field()
-  status: Status;
+    @Field()
+    status: Status
 }
