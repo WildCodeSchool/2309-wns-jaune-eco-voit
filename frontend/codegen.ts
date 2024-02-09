2289a3d93c86e3947a86a239a8d79b965b0f5ec6
lourima <marielou.lejan@gmail.com>
2024-02-09 11:32:53 +0100

    apolloclient front

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://backend:4000",
+  schema: "http://localhost:4000",
   documents: ["src/requetes/queries/*.queries.ts"],
   generates: {
     "./src/types/graphql.ts": {
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Login } from "./ecovoit/login/login";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Login/>
+      <Login />
     </main>
   );
 }
