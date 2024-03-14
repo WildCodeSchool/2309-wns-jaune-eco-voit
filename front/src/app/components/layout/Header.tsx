52f32f0a4e1485724009fb00c77dac39923deef2
Asma <asma.touzani7@gmail.com>
2024-03-14 22:34:14 +0100

    adding test front

--- front/src/app/components/layout/Header.tsx
+++ front/src/app/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { getCookie, CookieValueTypes } from "cookies-next";
 //Assets
-import logo from "@/assets/Logo.webp";
+// import logo from "@/assets/Logo.webp";
 import { Avatar, Button } from "@mui/material";
 import Link from "next/link";
 import { routes } from "@/app/lib/routes";
@@ -34,7 +34,7 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center py-6 px-6">
       <Link href={`${routes["home"].pathname}`}>
-        <Image src={logo} alt="Ecovoit" height={45} />
+        {/* <Image src={logo} alt="Ecovoit" height={45} /> */}
       </Link>
       <nav className="flex gap-4 items-center">
         {!userInfos?.email ? (
