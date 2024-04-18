b9ee519becd265a33b3ba9d2a9b8443dc3414fa2
Marie-Lou Le Jan <108528659+MarieLouLeJan@users.noreply.github.com>
2024-04-18 16:57:23 +0200

    Fix docker (#110)
    change title

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -13,7 +13,7 @@ export type UserInfos = {
 export default function Home() {
   return (
     <div className="home_page flex-1 flex flex-col gap-6 h-full items-center justify-center bg-primary10 py-10">
-      <h1>Il faut rouler cool !</h1>
+      <h1>Il faut rouler cool ma poule !</h1>
       <h3 className="font-medium uppercase tracking-widest">Ecoooooooovoit</h3>
       <div className="flex gap-3">
         <Button variant="outlined">outlined</Button>
