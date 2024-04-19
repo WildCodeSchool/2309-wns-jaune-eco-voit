96acc71b4664a7f1e0329526dbda2a8a523771d7
Ashitaaka <113708289+Ashitaaka@users.noreply.github.com>
2024-04-19 11:52:11 +0200

    Change title to check Deploy

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -13,7 +13,7 @@ export type UserInfos = {
 export default function Home() {
   return (
     <div className="home_page flex-1 flex flex-col gap-6 h-full items-center justify-center bg-primary10 py-10">
-      <h1>Il faut rouler cool wesh !</h1>
+      <h1>Il faut rouler cool Raoul !</h1>
       <h3 className="font-medium uppercase tracking-widest">Ecovoit</h3>
       <div className="flex gap-3">
         <Button variant="outlined">outlined</Button>
