b49d38c36dd89659868e0e3c891e4cca98d0cb0d
lourima <marielou.lejan@gmail.com>
2024-06-13 12:36:23 +0200

    acceptation bookings

--- back/src/index.ts
+++ back/src/index.ts
@@ -50,7 +50,7 @@ async function main() {
 
     // la variable job est necessaire pour créé le cron mais n'est jamais appelée a proprement parlé dans le code
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const job = schedule.scheduleJob('*/30 * * * *', async function () {
+    const job = schedule.scheduleJob('*/1 * * * *', async function () {
         await changeJourneysStatus()
     })
 
