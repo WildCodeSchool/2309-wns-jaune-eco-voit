32250db713e8ef6513c53300bef30f13fd73fe96
AlexandreOttmann <ottmann.alex@gmail.com>
2024-06-13 15:35:00 +0200

    Feature ok

---
+++ front/src/app/components/Buttons/BookJourneyButton.tsx
@@ -0,0 +1,67 @@
+import { JourneyEntity, useCreateBookingMutation } from "@/types/graphql";
+import { Button } from "@mui/material";
+
+import React, { useContext, useState } from "react";
+import { AuthContext } from "@/context/authContext";
+import { CreateBookingInput } from "@/types/graphql";
+import { useRouter } from "next/navigation";
+import { routes } from "@/app/lib/routes";
+
+const BookJourneyButton = ({ journey }: any) => {
+  const { getUser: userId } = useContext(AuthContext);
+
+  console.log("journey in btn", journey);
+  console.log("userId", userId);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const [
+    createBookingMutation,
+    {
+      data: createBookingSuccess,
+      loading: createBookingLoading,
+      error: createBookingError,
+    },
+  ] = useCreateBookingMutation();
+
+  const router = useRouter();
+  const BookJourney = () => {
+    setErrorMessage("");
+    const booking: CreateBookingInput = {
+      user: { id: userId! },
+      journey: { id: journey.id },
+      status: journey.status,
+    };
+    console.log("booking", booking);
+    createBookingMutation({
+      variables: { data: booking },
+      onCompleted: (res) => {
+        console.log("res", res);
+        setTimeout(() => {
+          router.push(`/booking/${res.createBooking.id}`);
+        }, 100);
+      },
+      onError: (err) => {
+        console.log("err", err.message);
+        setErrorMessage(err.message);
+      },
+    });
+  };
+
+  return (
+    <>
+      {userId ? (
+        <div className="flex justify-center">
+          <Button variant="contained" size="large" onClick={BookJourney}>
+            Réserver
+          </Button>
+          <div>{errorMessage}</div>
+        </div>
+      ) : (
+        <div className="text-center">
+          Connectez-vous pour pouvoir réserver ce trajet.
+        </div>
+      )}
+    </>
+  );
+};
+export default BookJourneyButton;
