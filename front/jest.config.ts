e77e339a5da21da72d94a69980f1b4bc23a69250
lourima <marielou.lejan@gmail.com>
2024-06-23 16:47:21 +0200

    fix

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -1,32 +1,5 @@
 import type { Config } from "jest";
 
-import type { JestConfigWithTsJest } from "ts-jest";
-
-// const jestConfig: JestConfigWithTsJest = {
-//   // [...]
-//   transform: {
-//     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-//     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-//     "^.+\\.tsx?$": [
-//       "ts-jest",
-//       {
-//         coverageProvider: "v8",
-//         setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-//         testEnvironment: "jsdom",
-//         transform: {
-//           "^.+.(ts|js)x?$": "ts-jest",
-//         },
-//         testMatch: ["**/_tests_/**/*.[jt]s?(x)"],
-//         moduleNameMapper: {
-//           "^@/(.*)$": "<rootDir>/src/$1",
-//         },
-//         preset: "ts-jest",
-//         testPathIgnorePatterns: ["\\node_modules\\", "<rootDir>/cypress/"],
-//       },
-//     ],
-//   },
-// };
-
 const config: Config = {
   coverageProvider: "v8",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
