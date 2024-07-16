c8ced27eb1826e152447d50e15f3ab39372b1584
lourima <marielou.lejan@gmail.com>
2024-07-16 12:15:15 +0200

    CI cypress

--- back/src/services/booking.service.ts
+++ back/src/services/booking.service.ts
@@ -1,6 +1,5 @@
 import { Repository } from 'typeorm'
 import datasource from '../db'
-import datasourceTest from '../db_test'
 import {
     BookingEntity,
     CreateBookingInput,
@@ -15,10 +14,10 @@ export default class BookingService {
     db: Repository<BookingEntity>
 
     constructor() {
-        this.db =
-            process.env.NODE_ENV === 'test'
-                ? datasourceTest.getRepository(BookingEntity)
-                : datasource.getRepository(BookingEntity)
+        // this.db =
+        //     process.env.NODE_ENV === 'test'
+        //         ? datasourceTest.getRepository(BookingEntity)
+        datasource.getRepository(BookingEntity)
     }
 
     async listBookings(): Promise<BookingEntity[]> {
--- front/cypress/support/commands.ts
+++ front/cypress/support/commands.ts
@@ -4,17 +4,17 @@ import "@testing-library/cypress/add-commands";
 declare global {
   namespace Cypress {
     interface Chainable {
-      register(): Chainable<void>;
-      login(): Chainable<void>;
+      register(email: string): Chainable<void>;
+      login(email: string, password: string): Chainable<void>;
     }
   }
 }
 
-Cypress.Commands.add("register", () => {
+Cypress.Commands.add("register", (email: string) => {
   cy.visit("/auth/register");
-  cy.url().should("eq", "http://localhost:3003/auth/register");
+  cy.url().should("eq", "http://localhost:3002/auth/register");
 
-  cy.findByRole("textbox", { name: "Email" }).type("sakogm38@gmail.com");
+  cy.findByRole("textbox", { name: "Email" }).type(email);
   // Les textbox de mot de passe ont un role différent
   cy.findByLabelText("Mot de passe").type("sakogm38");
   cy.findByLabelText("Confirmez le mot de passe").type("sakogm38");
@@ -22,18 +22,16 @@ Cypress.Commands.add("register", () => {
   cy.findByRole("textbox", { name: "Nom" }).type("ier");
   cy.findByRole("textbox", { name: "Date de naissance" }).type("07/05/1992");
 
-  cy.findByRole("button", { name: "S'inscrire" })
-    .contains("S'inscrire")
-    .click();
+  cy.findByRole("button", { name: "S'inscrire" }).click();
 });
 
-Cypress.Commands.add("login", () => {
+Cypress.Commands.add("login", (email: string, password: string) => {
   cy.visit("/auth/login");
-  cy.url().should("eq", "http://localhost:3003/auth/login");
+  cy.url().should("eq", "http://localhost:3002/auth/login");
 
-  cy.findByRole("textbox", { name: "Email" }).type("sakogm38@gmail.com");
-  cy.findByLabelText("Mot de passe").type("sakogm38");
+  cy.findByRole("textbox", { name: "Email" }).type(email);
+  cy.findByLabelText("Mot de passe").type(password);
   cy.findByRole("button", { name: "Se connecter" }).click();
 
-  cy.url().should("eq", "http://localhost:3003/");
+  cy.url().should("eq", "http://localhost:3002/");
 });
