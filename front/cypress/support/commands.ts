27fce7adaa1b3cd924663924d686f0baaa265d5a
lourima <marielou.lejan@gmail.com>
2024-07-17 18:03:36 +0200

    test

--- front/cypress/support/commands.ts
+++ front/cypress/support/commands.ts
@@ -12,7 +12,12 @@ declare global {
 
 Cypress.Commands.add("register", (email: string) => {
   cy.visit("/auth/register");
-  cy.url().should("eq", "http://localhost:3002/auth/register");
+  cy.url().should(
+    "eq",
+    process.env.CYPRESS_BASE_URL
+      ? `${process.env.CYPRESS_BASE_URL}/auth/register`
+      : "http://localhost:3002/auth/register"
+  );
 
   cy.findByRole("textbox", { name: "Email" }).type(email);
   // Les textbox de mot de passe ont un role différent
@@ -27,11 +32,21 @@ Cypress.Commands.add("register", (email: string) => {
 
 Cypress.Commands.add("login", (email: string, password: string) => {
   cy.visit("/auth/login");
-  cy.url().should("eq", "http://localhost:3002/auth/login");
+  cy.url().should(
+    "eq",
+    process.env.CYPRESS_BASE_URL
+      ? `${process.env.CYPRESS_BASE_URL}/auth/login`
+      : "http://localhost:3002/auth/login"
+  );
 
   cy.findByRole("textbox", { name: "Email" }).type(email);
   cy.findByLabelText("Mot de passe").type(password);
   cy.findByRole("button", { name: "Se connecter" }).click();
 
-  cy.url().should("eq", "http://localhost:3002/");
+  cy.url().should(
+    "eq",
+    process.env.CYPRESS_BASE_URL
+      ? process.env.CYPRESS_BASE_URL
+      : "http://localhost:3002"
+  );
 });
