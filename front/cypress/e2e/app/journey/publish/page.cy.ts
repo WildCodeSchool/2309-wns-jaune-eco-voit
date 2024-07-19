036774ceaa9a17ca08e7fdec151fcb303851fa7c
lourima <marielou.lejan@gmail.com>
2024-07-19 09:13:01 +0200

    test

--- front/cypress/e2e/app/journey/publish/page.cy.ts
+++ front/cypress/e2e/app/journey/publish/page.cy.ts
@@ -74,9 +74,7 @@ describe("Journey publish page", () => {
     /*------ Login ------*/
     cy.login("sakogm38@gmail.com", "sakogm38");
 
-    cy.get("body").contains("Vérifiez vos informations").should("be.visible");
-
-    cy.url({ timeout: 100000 }).should("eq", baseUrl);
+    cy.url({ timeout: 500000 }).should("eq", baseUrl);
 
     // waitFor(
     //   () => {
