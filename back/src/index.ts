48fa480017b4dd6c2f1dbf367d8d7dba77b8a9d5
lourima <marielou.lejan@gmail.com>
2024-07-22 09:20:20 +0200

    calcul

--- back/src/index.ts
+++ back/src/index.ts
@@ -45,7 +45,6 @@ export interface Payload {
 
 // Création d'un serveur HTTP à partir de la bibliothéque d'express
 const app = express()
-
 const httpServer = http.createServer(app)
 
 const bookingService = new BookingService()
