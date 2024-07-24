dce736eb166e034f403912badfa09a428923c783
Asma <asma.touzani7@gmail.com>
2024-07-24 12:25:41 +0200

    responsive card pending

--- front/src/app/components/onPending/OnPending.tsx
+++ front/src/app/components/onPending/OnPending.tsx
@@ -5,13 +5,12 @@ import {
     ListJourneysByUserQuery,
     UserEntity,
     useAcceptBookingMutation,
-    useListJourneysByUserQuery,
     useRejectBookingMutation,
 } from "@/types/graphql";
 import {
     TabPanel, Timeline, TimelineConnector, TimelineContent, TimelineItem, timelineItemClasses, TimelineSeparator
 } from "@mui/lab";
-import { Avatar, Button, Card, CardContent, Link, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import AvatarJourney from "../Avatar/AvatarJouney";
 import TripOriginOutlinedIcon from "@mui/icons-material/TripOriginOutlined";
@@ -111,7 +110,6 @@ const OnPendingTab = ({ journeys, journeysRefetch }: OnPendingTabProps) => {
         }
     }, [journeys]);
 
-    console.log("bookingsPending", bookingsPending)
     return (
         <>
             <TabPanel value="PENDING">
@@ -128,91 +126,94 @@ const OnPendingTab = ({ journeys, journeysRefetch }: OnPendingTabProps) => {
                             departureTime,
                             user: { profilePicture, firstname },
                         }) => (
-                            <div key={id} className="my_pending_card w-1/3 flex p-4 rounded-md shadow-md">
-                                <div className="w-full flex flex-col gap-3">
-                                    <div className="flex flex-col xs:flex-row w-full xs:justify-between gap-4">
-                                        <p className="text-base">
-                                            {formattedDate(departureTime)} à{" "}
-                                            {formattedTime(departureTime)}
-                                            <br />
-                                            <span className="text-sm text-dark60">
-                                                ({nbPassenger} siège{nbPassenger > 1 && "s"}{" "}
-                                                réservé{nbPassenger > 1 && "s"})
-                                            </span>
-                                        </p>
-                                        <p className="price text-sm px-3 py-1 rounded-md bg-primary100 text-white w-fit h-fit flex-shrink-0">
-                                            {price * nbPassenger} €
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className="status text-sm px-3 py-1 rounded-md bg-primary100 text-white w-fit h-fit flex-shrink-0 justify-self-end">
-                                            {statusFrench[status as Status]}
-                                        </p>
-                                    </div>
-                                    <div className="TimeLine flex items-center w-full border border-dark20 rounded-md p-4 h-fit">
-                                        <Timeline
-                                            sx={{
-                                                "ul.MuiTimeline-root": { padding: 0 },
-                                                ".MuiTimelineItem-root": { padding: 0, minHeight: 0 },
-                                                [`& .${timelineItemClasses.root}:before`]: {
-                                                    flex: 0,
-                                                    padding: 0,
-                                                },
-                                            }}
-                                        >
-                                            <TimelineItem className="h-20">
-                                                <TimelineSeparator>
+                            <div key={id} className="flex flex-wrap gap-4">
+                                <div className="my_pending_card w-full md:w-1/3 lg:w-1/2 flex p-4 rounded-md shadow-md ">
+                                    <div className="w-full flex flex-col gap-3">
+                                        <div className="flex flex-col xs:flex-row w-full xs:justify-between gap-4">
+                                            <p className="text-base">
+                                                {formattedDate(departureTime)} à{" "}
+                                                {formattedTime(departureTime)}
+                                                <br />
+                                                <span className="text-sm text-dark60">
+                                                    ({nbPassenger} siège{nbPassenger > 1 && "s"}{" "}
+                                                    réservé{nbPassenger > 1 && "s"})
+                                                </span>
+                                            </p>
+                                            <p className="price text-sm px-3 py-1 rounded-md bg-primary100 text-white w-fit h-fit flex-shrink-0">
+                                                {price * nbPassenger} €
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <p className="status text-sm px-3 py-1 rounded-md bg-primary100 text-white w-fit h-fit flex-shrink-0 justify-self-end">
+                                                {statusFrench[status as Status]}
+                                            </p>
+                                        </div>
+                                        <div className="TimeLine flex flex-col lg:flex-row  w-full border border-dark20 rounded-md p-4 h-fit gap-4">
+                                            <Timeline
+                                                sx={{
+                                                    "ul.MuiTimeline-root": { padding: 0 },
+                                                    ".MuiTimelineItem-root": { padding: 0, minHeight: 0 },
+                                                    [`& .${timelineItemClasses.root}:before`]: {
+                                                        flex: 0,
+                                                        padding: 0,
+                                                    },
+                                                }}
+                                            >
+                                                <TimelineItem className="h-20">
+                                                    <TimelineSeparator>
+                                                        <TripOriginOutlinedIcon
+                                                            fontSize="small"
+                                                            color="primary"
+                                                            sx={{ marginBottom: 0, padding: 0 }}
+                                                        />
+                                                        <TimelineConnector
+                                                            sx={{
+                                                                bgcolor: "primary.main",
+                                                                height: "200px",
+                                                                width: "3px",
+                                                            }}
+                                                        />
+                                                    </TimelineSeparator>
+                                                    <TimelineContent>
+                                                        <div>
+                                                            <p className="text-xs text-dark80">ORIGINE</p>
+                                                            <p>{origin}</p>
+                                                        </div>
+                                                    </TimelineContent>
+                                                </TimelineItem>
+                                                <TimelineItem className="h-fit">
                                                     <TripOriginOutlinedIcon
                                                         fontSize="small"
                                                         color="primary"
-                                                        sx={{ marginBottom: 0, padding: 0 }}
                                                     />
-                                                    <TimelineConnector
-                                                        sx={{
-                                                            bgcolor: "primary.main",
-                                                            height: "200px",
-                                                            width: "3px",
-                                                        }}
-                                                    />
-                                                </TimelineSeparator>
-                                                <TimelineContent>
-                                                    <div>
-                                                        <p className="text-xs text-dark80">ORIGINE</p>
-                                                        <p>{origin}</p>
-                                                    </div>
-                                                </TimelineContent>
-                                            </TimelineItem>
-                                            <TimelineItem className="h-fit">
-                                                <TripOriginOutlinedIcon
-                                                    fontSize="small"
-                                                    color="primary"
-                                                />
-                                                <TimelineContent>
-                                                    <div>
-                                                        <p className="text-xs text-dark80">DESTINATION</p>
-                                                        <p>{destination}</p>
-                                                    </div>
-                                                </TimelineContent>
-                                            </TimelineItem>
-                                        </Timeline>
+                                                    <TimelineContent>
+                                                        <div>
+                                                            <p className="text-xs text-dark80">DESTINATION</p>
+                                                            <p>{destination}</p>
+                                                        </div>
+                                                    </TimelineContent>
+                                                </TimelineItem>
+                                            </Timeline>
+                                        </div>
                                         <AvatarJourney
                                             id={id}
                                             firstname={firstname}
                                             profilePicture={profilePicture ?? undefined}
                                         />
-                                    </div>
-                                    <div className="card_footer flex flex-col item-start xs:flex-row xs:justify-between xs:items-end gap-3">
-                                        <div className="buttons flex gap-3 justify-content-center">
-                                            {status === "PENDING" && (
-                                                <>
-                                                    <Button onClick={() => handleAccept(id)}>Accepter</Button>
-                                                    <Button onClick={() => handleReject(id)}>Refuser</Button>
-                                                </>
-                                            )}
+                                        <div className="card_footer flex flex-col items-start xs:flex-row xs:justify-between xs:items-end gap-3">
+                                            <div className="buttons flex gap-3 justify-content-center">
+                                                {status === "PENDING" && (
+                                                    <>
+                                                        <Button onClick={() => handleAccept(id)}>Accepter</Button>
+                                                        <Button onClick={() => handleReject(id)}>Refuser</Button>
+                                                    </>
+                                                )}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
                         )
                     )
                 ) : (
